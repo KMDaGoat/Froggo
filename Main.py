4f379a8ac53ea8d9fec1550bae832b4c34355d91
Aliff Abedin <aliffahnaff2020@gmail.com>
2026-01-18 21:04:54 +0000

    Refactor tile image loading and clean comments
    Removed redundant comments and adjusted image loading for tiles.

---
+++ Main.py
@@ -0,0 +1,154 @@
+from pygame import *
+import random
+width , height =  720 , 1080
+screen = display.set_mode((width , height))
+colourcliff =  (20, 80, 20)
+
+tilewidth , tilelength = 80 , 64
+class tile:
+    def __init__(self , x , y , image ,extraheight = 0 , flip = False , overlay = False):
+        if flip:
+            image = transform.scale(image, (tilewidth, tilelength + extraheight))
+            image = transform.flip(image , False , True)
+            self.img = Surface((tilewidth, tilelength + extraheight))
+            self.img.blit(image, (0, 0))
+        else :
+            self.img = Surface((tilewidth, tilelength + extraheight))
+            image = transform.scale(image , (tilewidth, tilelength + extraheight))
+            self.img.blit(image , (0,0))
+            # Add a small highlight on top of cliffs to show the edge
+            if extraheight > 0:
+                draw.rect(self.img, (105, 138, 0), (0, 0, 64, 5))
+            elif overlay:
+                draw.rect(self.img, (105, 138, 0), (0, 0, 64, 5))
+
+        self.rect = self.img.get_rect(topleft=(x, y))
+
+
+#make the actual object which handles the plaer as well as trees:
+class treeorplayer:
+    def __init__(self , x , y , image , base , length ):
+        image = transform.scale(image, (base, length))
+        colourremoval = image.get_at((0, 0))
+        image.set_colorkey(colourremoval)
+
+        self.img = Surface((base , length) , SRCALPHA)
+        self.img.blit(image , (0,0))
+        self.rect = Rect(x , y , base , length )#make a rectangle for the shadow to attach to
+        self.speed = 4
+
+    def shadow(self , screen):
+        shadowappearance = Surface((self.rect.width , 14) ,SRCALPHA)
+        draw.ellipse(shadowappearance , (0,0,0,90) , [0,0,self.rect.width , 14 ])
+        screen.blit(shadowappearance , (self.rect.x , self.rect.bottom - 7))
+
+    def playermovement(self):
+        movement = 64
+        lowerlim = -64
+        for e in event.get():
+            if e.type == KEYDOWN:
+                if e.key == K_w:
+
+                    self.rect.y -= movement
+                    if self.rect.y < lowerlim:
+                        self.rect.y += movement
+                if e.key == K_a:
+                    self.rect.x -= movement
+                    if self.rect.x < lowerlim:
+                        self.rect.x += movement
+                if e.key == K_s:
+                    self.rect.y += movement
+                    if self.rect.y > height:
+                        self.rect.y -= movement
+                if e.key == K_d:
+                    self.rect.x += movement
+                    if self.rect.x >= width:
+                        self.rect.x -= movement
+
+colwise = 11
+tilemap = [
+    ["G"] * colwise,
+    ["R"] * colwise,
+    ["R"] * colwise,
+    ["R"] * colwise,
+    ["R"] * colwise,
+    ["G"] * colwise,
+    ["C"] * colwise,
+    ["W"] * colwise,
+    ["W"] * colwise,
+    ["W"] * colwise,
+    ["C"] * colwise,
+    ["G"] * colwise,
+    ["R"] * colwise,
+    ["R"] * colwise,
+    ["R"] * colwise,
+    ["R"] * colwise,
+    ["G"] * colwise,
+
+
+]
+
+waterlayer =[]
+groundlayer = []
+
+
+for index , item in enumerate(tilemap):
+    for multiplier , col in enumerate(item):
+        xpos , ypos = multiplier * 64 , index * 64
+        if col == "G":
+            pic = image.load(r"C:\Users\aliff\Downloads\grassone.png")
+            groundlayer.append(tile(xpos , ypos , pic))
+            if index == 11:
+                pic = image.load(r"C:\Users\aliff\Downloads\grassone.png")
+                groundlayer.append(tile(xpos, ypos, pic , 0 , False , True))
+        if col == "R":
+            pic = image.load(r"C:\Users\aliff\OneDrive\Desktop\froggoassets\PNG\Default\roadTexture_13.png")
+            groundlayer.append(tile(xpos, ypos, pic))
+        elif col == "W":
+            pic = image.load(r"C:\Users\aliff\Downloads\water.png")
+            waterlayer.append(tile(xpos, ypos, pic))
+        elif col == "C":
+            if index == 10:
+                pic = image.load(r"C:\Users\aliff\Downloads\cliff.png")
+                groundlayer.append(tile(xpos, ypos, pic, 25 , True))
+            else:
+                pic = image.load(r"C:\Users\aliff\Downloads\cliff.png")
+                groundlayer.append(tile(xpos , ypos , pic , 25))
+
+pic = image.load(r"C:\Users\aliff\Downloads\frog.png")
+player = treeorplayer( (width / 2) , height , pic ,  64, 64  )
+
+pic = image.load(r"C:\Users\aliff\Downloads\tree.png")
+trees = []
+
+numberoftrees = 15
+for i in range(numberoftrees):
+    xpos = random.randint(1,720)
+    xpos = (xpos // 64) * 64
+    ypos = random.randint(1, 1080)
+    ypos = (ypos // 64) * 64
+    trees.append(treeorplayer(xpos, ypos, pic, 40, 45))
+
+
+
+
+endgame = False
+while not endgame:
+
+    player.playermovement()
+    for e in  event.get():
+        if e.type == QUIT:
+            endgame = True
+
+    for tile in waterlayer:
+        screen.blit(tile.img , tile.rect)
+    for tile in groundlayer:
+        screen.blit(tile.img , tile.rect)
+
+    depth_elements = [player] + trees
+    #sorts the list out in order of lowest to highest yaxis vals
+    depth_elements.sort(key=lambda obj: obj.rect.bottom)
+    for obj in depth_elements:
+        obj.shadow(screen)
+        screen.blit(obj.img ,  obj.rect)
+    display.update()
